4038c217149c00105bb630ff6c66d8fc5ef696d6
Meelunae <git@eleuna.me>
2023-06-04 02:45:08 +0200

    Command prefix configuration

--- bot.py
+++ bot.py
@@ -13,14 +13,15 @@ class Bot(commands.Bot):
 intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(">"), 
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(cfg.configured_prefix), 
                    description='Music streaming bot based on YTDL for various platforms', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user} - {bot.user.id})')
-    print('------')
-    print(f'Bot owner ID is {cfg.owner_id}')
+    print(f"Logged in as {bot.user} - {bot.user.id})")
+    print("------")
+    print(f"Bot owner ID is {cfg.owner_id}")
+    print(f"Configured command prefix is {cfg.configured_prefix}")
     await bot.add_cog(Music(bot))
 
 def main():
--- config.py
+++ config.py
@@ -8,16 +8,20 @@ class Config:
         self.kaori_token = os.getenv("KAORI_AUTH_TOKEN")
         self.kaori_id = int(os.getenv("KAORI_ID"))
         self.owner_id = int(os.getenv("OWNER_ID"))
+        self.configured_prefix = os.getenv("COMMAND_PREFIX") or "!"
 
         if self.kaori_token is None:
-            self.cfg_error("kaori_token")
+            self.cfg_error("KAORI_AUTH_TOKEN")
 
         if self.kaori_id is None:
-            self.cfg_error("kaori_id")
+            self.cfg_error("KAORI_ID")
 
         if self.owner_id is None:
-            self.cfg_error("owner_id")
+            self.cfg_error("OWNER_ID")
         
+        if self.configured_prefix is None:
+            self.cfg_error("COMMAND_PREFIX")
+
     def cfg_error(key):
         print(f"ERROR: .env file is not configured properly, missing {key}")
 
